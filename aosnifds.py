26e9e2453bc4b65b5b4283f3e0d56b7e9b449b99
jayaprakash <jayaprakash0394@gmail.com>
2025-02-12 15:40:14 +0530

    Create aosnifds.py

---
+++ aosnifds.py
@@ -0,0 +1,10 @@
+def welcome_message():
+    print("Welcome to Tech Week Session")
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    welcome_message()
+    result = add_numbers(5, 3)
+    print(f"The result of adding 5 and 3 is: {result}")
