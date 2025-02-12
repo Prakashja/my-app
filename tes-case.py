4044a12ca151fb5af3c38f8d24d95e462cbf2fc4
jayaprakash <jayaprakash0394@gmail.com>
2025-02-12 15:56:17 +0530

    [ci skip]

--- tes-case.py
+++ tes-case.py
@@ -1,8 +1,15 @@
+"""
+Module test_case
+"""
+
 import unittest
 from tech_week_session import add_numbers
 
 class TestTechWeekSession(unittest.TestCase):
+    """Unit tests for Tech Week Session"""
+
     def test_add_numbers(self):
+        """Test the add_numbers function"""
         self.assertEqual(add_numbers(5, 3), 8)
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
