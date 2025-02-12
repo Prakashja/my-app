97cdddbec624af8ab39113f13d7c5c578dee48ad
jayaprakash <jayaprakash0394@gmail.com>
2025-02-12 15:51:19 +0530

    [skip ci]

--- aosnifds.py
+++ aosnifds.py
@@ -1,10 +1,16 @@
+"""
+Module aosnifds
+"""
+
 def welcome_message():
+    """Prints a welcome message."""
     print("Welcome to Tech Week Session")
 
 def add_numbers(a, b):
+    """Adds two numbers and returns the result."""
     return a + b
 
 if __name__ == "__main__":
     welcome_message()
-    result = add_numbers(5, 3)
-    print(f"The result of adding 5 and 3 is: {result}")
+    RESULT = add_numbers(5, 3)
+    print(f"The result of adding 5 and 3 is: {RESULT}")
